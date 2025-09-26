8ed58b6e63eb8beaa65928546a2baff5aa5db2a3
matthiassouthwick <southwickmatthias@gmail.com>
2025-09-25 21:16:52 -0600

    useUserInfo & useUserInfoActions hooks

--- tweeter-web/src/components/mainLayout/UserItemScroller.tsx
+++ tweeter-web/src/components/mainLayout/UserItemScroller.tsx
@@ -1,16 +1,11 @@
 import React from "react";
-
-import { useContext } from "react";
-import {
-  UserInfoContext,
-  UserInfoActionsContext,
-} from "../userInfo/UserInfoContexts";
 import { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { AuthToken, FakeData, User } from "tweeter-shared";
 import { useParams } from "react-router-dom";
 import UserItem from "../userItem/UserItem";
 import { useMessageActions } from "../toaster/MessageHooks";
+import { useUserInfo, useUserInfoActions } from "../userInfo/UserInfoHooks";
 
 export const PAGE_SIZE = 10;
 
@@ -38,8 +33,8 @@ const UserItemScroller: React.FC<Props> = ({
   const addItems = (newItems: User[]) =>
     setItems((previousItems) => [...previousItems, ...newItems]);
 
-  const { displayedUser, authToken } = useContext(UserInfoContext);
-  const { setDisplayedUser } = useContext(UserInfoActionsContext);
+  const { displayedUser, authToken } = useUserInfo();
+  const { setDisplayedUser } = useUserInfoActions();
   const { displayedUser: displayedUserAliasParam } = useParams();
 
   // Update the displayed user context variable whenever the displayedUser url parameter changes. This allows browser forward and back buttons to work correctly.
